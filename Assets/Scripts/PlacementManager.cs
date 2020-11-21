00fdcfd6f3382b4dcffb7e7514242afba6cf14d7
Ariions <arnas.bendinskas@gmail.com>
2020-11-21 19:25:50 +0200

    SPRITES and more sripts
    altho its not funtional yet buge leep towards tommorow ehre it works

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -2,49 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO:
+//  - dont check for bear
+//  - dont double check false ones
+//  - contact animation to indicate possible merge
+//  - work with input
+//  - work with visuals
+//  - crystal upgrade mechanics
+   
 public class PlacementManager : MonoBehaviour
 {
-    public struct Coordinates
-    {
-        public int x;
-        public int y;
-    }
 
-    private int xAxis = 6;
-    private int yAxis = 6;
+    // as i am making recreation of this game, i am just cutting these in stone
+    private const int xAxis = 6;
+    private const int yAxis = 6;
 
     Tile[][] tile;
+    bool[][] visited;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // inicialize all tiles and save their references
-        for(int i = 0; i < xAxis; i++)
-        {
-            for(int j = 0; j < yAxis; j++)
-            {
-                tile[i][j] = new Tile();
-                tile[i][j].tileCoordinates.x = i;
-                tile[i][j].tileCoordinates.y = j;
-            }
-        }
-    }
+    Tile currentPlaceableObject;
 
-    // Update is called once per frame
-    void Update()
+    private int TotalSame = 0;
+
+    private void Start()
     {
+        // setting values for visited array
         
     }
 
-
-    void placeTile(Coordinates spot, Tile.Type _type)
+    public void SelectedTile(Tile tileToCheck)
     {
-        tile[spot.x][spot.y].tileType = _type;
+        if (!tileToCheck.isEmpty())
+        {
+            
+        }
     }
 
+    public bool CheckForUpgrades(Tile tileToCheck)
+    {
+        if (checkForSameAndMark(tileToCheck, currentPlaceableObject) > 2)
+        {
+            return true;
+        }
 
+        return false;
+    }
 
-    void UpgradeTile(Tile upgradedTile, Tile[] sacrificedTiles)
+    public void CreateReference(Tile tileref)
+    {
+        tile[tileref.coordinates.x][tileref.coordinates.y] = tileref;
+    }
+
+    void placeTile(Tile.Coordinates coordinates, Tile.Type tileType)
+    {
+        tile[coordinates.x][coordinates.y].tileType = tileType;
+    }
+
+    void UpgradeTile(Tile upgradableTile, Tile[] sacrificedTiles)
     {
 
         foreach(Tile t in sacrificedTiles)
@@ -52,21 +66,87 @@ public class PlacementManager : MonoBehaviour
             t.ClearTile();
         }
 
-        upgradedTile.Upgrade();
+        upgradableTile.Upgrade();
     }
 
     // check if it is possible to upgrade current tile then returns all of the tiles that are going to be consumed for an upgrade
-    Tile[] upgradbleTiles(Coordinates spot, Tile.Type _type) 
+    void upgradbleTiles(Tile upgradableTile) 
     {
-        
+        // if(upgradableTile)
+      
+     
 
 
-        return null;
     }
 
-    void Dig(Tile.Type _type)
-    {
 
+    // get amount and changes visited array of all the tile that are the same type and are adjust to each other
+    int checkForSameAndMark(Tile centerTile, Tile CompareToTile)
+    {
+        int sameAmount = 0;
+        #region check side tile and visit
+        //left
+        if (centerTile.coordinates.x > 0) //if not on a border
+            if (!visited[centerTile.coordinates.x - 1][centerTile.coordinates.y] && tile[centerTile.coordinates.x - 1][centerTile.coordinates.y].tileType == CompareToTile.tileType)
+            {
+                if (tile[centerTile.coordinates.x][centerTile.coordinates.y + 1].level == CompareToTile.level)
+                    Visit(tile[centerTile.coordinates.x - 1][centerTile.coordinates.y]);
+            }
+        
+        //right
+        if (centerTile.coordinates.x < xAxis - 1) //if not on a border
+            if (!visited[centerTile.coordinates.x + 1][centerTile.coordinates.y] && tile[centerTile.coordinates.x + 1][centerTile.coordinates.y].tileType == CompareToTile.tileType)
+            {
+                if (tile[centerTile.coordinates.x][centerTile.coordinates.y + 1].level == CompareToTile.level)
+                    Visit(tile[centerTile.coordinates.x + 1][centerTile.coordinates.y]);
+            }
+        
+        //top
+        if (centerTile.coordinates.y > 0) //if not on a border
+            if (!visited[centerTile.coordinates.x][centerTile.coordinates.y - 1] && tile[centerTile.coordinates.x][centerTile.coordinates.y - 1].tileType == CompareToTile.tileType)
+            {
+                if (tile[centerTile.coordinates.x][centerTile.coordinates.y + 1].level == CompareToTile.level)
+                    Visit(tile[centerTile.coordinates.x][centerTile.coordinates.y - 1]);
+            }
+
+        //bottom
+        if (centerTile.coordinates.y < yAxis - 1) //if not on a border
+            if (!visited[centerTile.coordinates.x][centerTile.coordinates.y + 1] && tile[centerTile.coordinates.x][centerTile.coordinates.y + 1].tileType == CompareToTile.tileType)
+            {
+                if(tile[centerTile.coordinates.x][centerTile.coordinates.y + 1].level == CompareToTile.level)
+                    Visit(tile[centerTile.coordinates.x][centerTile.coordinates.y + 1]);
+            }
+        #endregion
+
+        // maked tile as visited and and call parent method to look for the same type of tiles nearby
+        void Visit(Tile tileTovisit)
+        {
+            visited[tileTovisit.coordinates.x][tileTovisit.coordinates.y] = true; // mark as visited TODO: make so false tiles are marked as visited also
+            sameAmount = 1 + checkForSameAndMark(tile[tileTovisit.coordinates.x][tileTovisit.coordinates.y], CompareToTile); // 1 (this tile) + other adjacent tiles
+        }
+        return sameAmount;
+    }
+
+    void resetVisited()
+    {
+        for (int i = 0; i < xAxis; i++)
+        {
+            for (int j = 0; j < yAxis; j++)
+            {
+                visited[i][j] = false;
+            }
+        }
+    }
+
+    Tile UpgradesInto(Tile CheckedTile, bool fourOrMore = false)
+    {
+        Tile AnswerTileClone = new Tile();
+
+
+
+
+
+        return AnswerTileClone;
     }
 
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// TODO: Move dictionary to file
+
 public class Tile : MonoBehaviour
 {
     public struct Coordinates
@@ -16,12 +18,19 @@ public class Tile : MonoBehaviour
         Holy,
         Crystal,
         Wood,
-        Bear
+        Bear,
+        Box,
+        Robot
     }
 
-    public int level = 0;                       // building level that is on top of this tile
+    string[][] namesDictionary; // move to JsonFIle if you have time
+
+    public string name = "Empty";
+
+    bool specialVersion = false;
+    public int level = 0;   // building level that is on top of this tile                    
     public Type tileType = Type.Empty;          
-    public Coordinates tileCoordinates;
+    public Coordinates coordinates;
 
     // clear any buildings from the tile and sets building level to 0
     public void ClearTile() 
@@ -39,7 +48,104 @@ public class Tile : MonoBehaviour
     {
         if(tileType == Type.Empty)
             return  true;
-
         return false;
     }
+
+    void updateInformation()
+    {
+        // update names bbased on level and fix levels if needed (is object cannot have other levels)
+        switch (tileType)
+        {
+            case Type.Wood:
+                switch (level)
+                {
+                    case 0:
+                        name = "Grass";
+                        break;
+                    case 1:
+                        name = "Bush";
+                        break;
+                    case 2:
+                        name = "Tree";
+                        break;
+                    case 3:
+                        name = "Hut";
+                        break;
+                    case 4:
+                        name = "House";
+                        break;
+                    case 5:
+                        name = "Castle";
+                        break;
+                    case 6:
+                        name = "Floating House";
+                        break;
+                    case 7:
+                        name = "Floating Castle";
+                        break;
+                    default:
+                        Debug.LogError("Tile :" + coordinates.x + "," + coordinates.y + " level was incorrect");
+                    break;
+                }
+                break;
+            case Type.Empty:
+                name = "Empty";
+                break;
+            case Type.Holy:
+                switch (level)
+                {
+                    case 0:
+                        name = "Tombstone";
+                        break;
+                    case 1:
+                        name = "Church";
+                        break;
+                    case 2:
+                        name = "Catherdral";
+                        break;
+                    default:
+                        Debug.LogError("Tile :" + coordinates.x + "," + coordinates.y + " level was incorrect");
+                        break;
+                }
+                break;
+            case Type.Crystal:
+                switch (level)
+                {
+                    case 0:
+                        name = "Rock";
+                        break;
+                    case 1:
+                        name = "Big rock";
+                        break;
+                    default:
+                        Debug.LogError("Tile :" + coordinates.x + "," + coordinates.y + " level was incorrect");
+                        break;
+                }
+                break;
+            case Type.Bear:
+
+                break;
+            case Type.Box:
+                switch (level)
+                {
+                    case 0:
+                        name = "Box";
+                        break;
+                    case 1:
+                        name = "Big box";
+                        break;
+                    default:
+                        Debug.LogError("Tile :" + coordinates.x + "," + coordinates.y + " level was incorrect");
+                        break;
+                }
+                break;
+            case Type.Robot:
+                name = "Robot";
+                level = 0;
+                break;
+            default:
+                Debug.LogError("Tile :" + coordinates.x + "," + coordinates.y + " Type was incorrect");
+                break;
+        }
+    } 
 }
